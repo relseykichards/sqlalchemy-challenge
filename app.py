c8ae54548cdb840ee28e2b9397c4902e8b3921b4
relseykichards <kelsrichards28@gmail.com>
2023-05-09 10:07:33 -0500

    added first query to app.py

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 # Import the dependencies.
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite", echo=False)
 
+# reflect an existing database into a new model
+Base = automap_base()
+Base.metadata.create_all(engine)
+# reflect the tables
+Base.prepare(autoload_with=engine)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # reflect an existing database into a new model
 
@@ -16,15 +32,51 @@
 
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+ 
+    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date <= '2017-08-23').filter(Measurement.date >= '2016-08-23').order_by(Measurement.date.desc()).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list 
+    precip_twelve_months = []
+    for date, prcp in results:
+        precip_dict = {}
+        precip_dict["date"] = date
+        precip_dict["prcp"] = prcp
+        precip_twelve_months.append(precip_dict)
+
+    return jsonify(precip_twelve_months)
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
